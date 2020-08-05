cbe75f4cd2e42c1b0705f18e2c5dfd154007ca60
Cano <david.cano.nieto@gmail.com>
2020-08-05 14:41:34 +0100

    [ADF-3499] Make Card View Text Item reactive to user input

--- lib/core/card-view/card-view.module.ts
+++ lib/core/card-view/card-view.module.ts
@@ -17,7 +17,7 @@
 
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -50,6 +50,7 @@ import { CardViewArrayItemComponent } from './components/card-view-arrayitem/car
     imports: [
         CommonModule,
         FormsModule,
+        ReactiveFormsModule,
         FlexLayoutModule,
         TranslateModule,
         MatDatepickerModule,
